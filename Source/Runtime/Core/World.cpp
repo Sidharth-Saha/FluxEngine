b8e5054097df77eb34407841c80dc8f5e0ac92dc
Sidharth Saha <ssaha2000@gmail.com>
2022-10-01 01:34:36 -0700

    Create World Update function.

--- Source/Runtime/Core/World.cpp
+++ Source/Runtime/Core/World.cpp
@@ -1,1 +1,12 @@
 #include "World.h"
+
+namespace Flux
+{
+	void World::Update(float deltaSeconds)
+	{
+		for (auto system : m_Systems)
+		{
+			system->Update(m_Registry, deltaSeconds);
+		}
+	}
+}
--- Source/Runtime/Core/World.h
+++ Source/Runtime/Core/World.h
@@ -1,12 +1,20 @@
 #pragma once
+#include "System.h"
+
 #include <entt/entity/registry.hpp>
+
+#include <vector>
 
 namespace Flux
 {
 	/** Class representing game world */
 	class FLUX_API World
 	{
+	public:
+		void Update(float deltaSeconds);
+
 	private:
 		entt::registry m_Registry;
+		std::vector<System*> m_Systems;
 	};
 }
